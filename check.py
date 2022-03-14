5fe57d1ae355f48be2b697e3fb8d0bb82e3eb66c
cgi-bin/ <6095048+sven-hash@users.noreply.github.com>
2022-03-14 08:31:17 +0100

    Create check.py

---
+++ check.py
@@ -0,0 +1,28 @@
+from hashlib import blake2b
+import base58
+import sys
+import requests
+
+pubkey = b'\x00' +  blake2b(bytes.fromhex(sys.argv[1]), digest_size=32).digest()
+addressDecoded=base58.b58encode(pubkey).decode()
+addressSign = sys.argv[2]
+
+print(f"\nAddress who signed: {addressDecoded}")
+print(f"Address to mark: {addressSign}")
+
+nameHex=sys.argv[3]
+
+data = {
+        "data": nameHex,
+        "signature": sys.argv[4],
+        "publicKey": sys.argv[1]
+}
+
+asciiName=bytes.fromhex(nameHex).decode("ASCII") 
+                        
+
+r = requests.post("https://mainnet-wallet.alephium.org/utils/verify-signature",json=data)
+
+print(f"\nIs signature valid: {r.text}")
+print(f"Is address legitimate: {addressDecoded == addressSign}")
+print(f"Data: {asciiName}")
